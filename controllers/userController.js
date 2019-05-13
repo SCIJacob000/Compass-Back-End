c046d77f1afe73843f4c2dcb56150895fe328fbf
Jacob Stolberg <jacobstolberg@Jacobs-MacBook-Air.local>
2019-05-13 11:33:34 -0500

    register route working login route debugging

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,77 @@ const bcrypt = require('bcryptjs');
 
 
 
+// Post/register this route will allow users to register an acct.
+router.post('/register', async (req,res)=>{
+	console.log(req.body);
+	console.log("you are hitting the route");
+	const password = req.body.password
+	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+	const userDBEntry = {};
+
+	userDBEntry.username = req.body.username
+	userDBEntry.password = passwordHash
+
+
+
+	try{
+		const createdUser = await User.create(userDBEntry);
+		await createdUser.save()
+		
+		req.session.logged = true
+		req.session.username = userDBEntry.username
+
+
+		res.json({
+			status: 200,
+			data: "login success"
+		})
+	}catch(error){
+		req.session.message = "Username or Password is incorrect!"
+		res.send(error)
+	}
+})
+
+
+
+
+// Post/login this route allows returning users to log back in
+
+router.post('/login', async(req,res, next)=>{
+	try{
+		const foundUser = User.findOne({'username': req.body.username})
+
+		if(foundUser !== null){
+			if(bcrypt.compareSync(req.body.password, foundUser.password)=== true){
+				req.session.logged = true;
+				req.session.userDbId = foundUser._id
+				res.status(200).json({
+					status:200
+				})
+			}
+			else{
+				req.session.message = "Username or password incorrect"
+			}
+		}
+	}catch(error){
+		next(error)
+	}
+})
+
+
+
+
+// Put/{username} this route will allow users to update their acct info  
+
+
+
+
+
+
+
+// Delete/logout this route allows users to logout/destroys session
+
 
 
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const session = require('express-session')
 const app = express();
 require('dotenv').config()
+const superagent = require('superagent');
 
 //require db
 require ('./db/db.js')
@@ -14,29 +15,71 @@ const PORT = process.env.PORT
 
 //require controllers
 
-const userController = require('./controllers/userController')
-const parkController = require('./controllers/parkController')
-const tripController = require('./controllers/tripController')
 
 //middlewares
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(methodOverride('_method'));
+app.use(bodyParser.json())
+
 app.use(session({
 	secret:process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: false
 }))
 
+const userController = require('./controllers/userController')
+const parkController = require('./controllers/parkController')
+const tripController = require('./controllers/tripController')
 
 
+app.use('/users', userController);
 
-app.use((req,res, next) => {
-	if (!req.session.logged) {
-		res.redirect('/users/login')
-	} else{
-		next()
-	}
+// this will be refactored to account for the logged in features!
+// app.use((req,res, next) => {
+// 	if (!req.session.logged) {
+// 		res.redirect('/users/login')
+// 	} else{
+// 		next()
+// 	}
+// })
+
+//Get/alerts this route gets all alerts for all nat parks
+
+app.get('/alerts', (req,res,next)=>{
+	superagent.get('https://developer.nps.gov/api/v1/alerts?api_key=' + process.env.API_KEY)
+	.then((data)=>{
+
+		const actualData = data.body.data//.data.articles.map(a => a.author)
+      	const justTheDataWeNeed = actualData.map(article => {
+      		return {
+		         title: article.title,
+		         description: article.description,
+		         category: article.category
+	        }
+	    })
+		res.status(200).json({
+			status: 200,
+			data: justTheDataWeNeed,
+			description: data.body.data.description,
+			category: data.body.data.category
+		})
+	}).catch((error)=>{
+		res.status(400).json({
+			status: 400,
+			error: error
+		})
+	})
 })
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
 	console.log('app is listening on port: ', PORT);
